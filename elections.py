a5ae614de7b8418089c8484b1bcc30146d7c3d18
Anne Williams <mawilliams098@gmail.com>
2020-07-30 19:44:00 -0400

    renamed csv columns

--- elections.py
+++ elections.py
@@ -3,7 +3,6 @@
 # how to load in a saved html file instead of requesting one: https://stackoverflow.com/questions/21570780/using-python-and-beautifulsoup-saved-webpage-source-codes-into-a-local-file
 
 import csv
-import requests
 from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(open('va_elections.html'), 'html.parser')
@@ -22,34 +21,31 @@ for row in table_body.find_all('tr', class_="election_item"):
     district = office.find_next_sibling()
     sub_table_container = row.find("td", class_="candidates_container_cell")
 
-
     single_year["Year"] = year.text
     single_year["Office"] = office.text
     single_year["District"] = district.text
     single_year["Winner"] = sub_table_container.div.span.text
 
-    # enter the drop down tables in each row of the main table
+    # enter the drop down tables in each row of the main table only for the winning candidate
     for sub_row in sub_table_container.find_all('tr', class_="is_winner"):
 
-        # print(sub_row.td.find('div', class_="name").text)
-        # print(sub_row.td.find('div', class_="party").text)
-        # print(sub_row.find_all('td', class_="number"))
-
+        # find_all('td, class_="number") returns a list containing candidate's total votes total votes represented as a percentage
+        # pick total votes and percentage from list & remove commas and % from respective elements
         total_votes_str = sub_row.find_all('td', class_="number")[0].text.replace(",", "")
         votes_percentage_str = sub_row.find_all('td', class_="number")[1].text[:-1]
-        # print(votes_percentage_str[:-1])
 
-        single_year["Winner Party"] = sub_row.td.find('div', class_="party").text
-        single_year["Winner Total Votes"] = int(total_votes_str)
-        single_year["Winner Vote %"] = float(votes_percentage_str)
+        # add party, total votes, and % of total votes to the dictionary for a single year's election
+        single_year['Party'] = sub_row.td.find('div', class_="party").text
+        single_year['Vote Count'] = int(total_votes_str)
+        single_year['Percent of Total Votes'] = float(votes_percentage_str)
 
-    print(single_year)
+    # add results from each row to the main list
     election_results.append(single_year)
 
-
+# convert election_results list of dictionaries into csv
 filename = 'elections_results.csv'
 with open(filename, 'w', newline='') as f: 
-    w = csv.DictWriter(f,['Year', 'Office', 'District', 'Winner', 'Winner Party', 'Winner Total Votes', 'Winner Vote %']) 
+    w = csv.DictWriter(f,['Year', 'Office', 'District', 'Winner', 'Party', 'Vote Count', 'Percent of Total Votes']) 
     w.writeheader() 
     for year in election_results: 
         w.writerow(year)

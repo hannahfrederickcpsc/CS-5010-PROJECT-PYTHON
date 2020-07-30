33f79b751317c27bca456a9e9f5aab8fd3915b03
Anne Williams <mawilliams098@gmail.com>
2020-07-30 19:32:49 -0400

    scraped vote counts from subtable on each row of main table

--- elections.py
+++ elections.py
@@ -20,19 +20,36 @@ for row in table_body.find_all('tr', class_="election_item"):
     year = row.find("td", class_="year first")      # td = cell in table row
     office = year.find_next_sibling()
     district = office.find_next_sibling()
-    winner_container = row.find("td", class_="candidates_container_cell")
+    sub_table_container = row.find("td", class_="candidates_container_cell")
+
 
     single_year["Year"] = year.text
     single_year["Office"] = office.text
     single_year["District"] = district.text
-    single_year["Winner"] = winner_container.div.span.text
+    single_year["Winner"] = sub_table_container.div.span.text
 
+    # enter the drop down tables in each row of the main table
+    for sub_row in sub_table_container.find_all('tr', class_="is_winner"):
+
+        # print(sub_row.td.find('div', class_="name").text)
+        # print(sub_row.td.find('div', class_="party").text)
+        # print(sub_row.find_all('td', class_="number"))
+
+        total_votes_str = sub_row.find_all('td', class_="number")[0].text.replace(",", "")
+        votes_percentage_str = sub_row.find_all('td', class_="number")[1].text[:-1]
+        # print(votes_percentage_str[:-1])
+
+        single_year["Winner Party"] = sub_row.td.find('div', class_="party").text
+        single_year["Winner Total Votes"] = int(total_votes_str)
+        single_year["Winner Vote %"] = float(votes_percentage_str)
+
+    print(single_year)
     election_results.append(single_year)
 
 
 filename = 'elections_results.csv'
 with open(filename, 'w', newline='') as f: 
-    w = csv.DictWriter(f,['Year', 'Office', 'District', 'Winner']) 
+    w = csv.DictWriter(f,['Year', 'Office', 'District', 'Winner', 'Winner Party', 'Winner Total Votes', 'Winner Vote %']) 
     w.writeheader() 
     for year in election_results: 
         w.writerow(year)
